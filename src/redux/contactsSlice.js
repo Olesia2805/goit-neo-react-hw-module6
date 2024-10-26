938dde0a989febb3e69b7266b13f732436e5ec8a
Olesia Vysotina <vysotina2002@gmail.com>
2024-10-26 20:31:03 +0200

    added logic to the contactsSlice.js

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { items: [] };
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, { payload }) => {
+      state.items.push(payload);
+    },
+    deleteContact: (state, { payload }) => {
+      state.items.filter(item => {
+        item.id != payload;
+      });
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
+
+export const selectContacts = state => state.contacts.items;
